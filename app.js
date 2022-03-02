8f66d4ffe6666c37fb0113094de43f27a09b5944
Victor Freitas <anafreitas.victor@yahoo.com>
2022-03-02 11:50:57 +0100

    crash fixed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 //requiring mail chimp 
-const mailchimp = require("@mailchimp/mailchimp_marketing");
+const client = require("@mailchimp/mailchimp_marketing");
 
 //https request
 const https = require('https');
@@ -17,7 +17,7 @@ const port = 3000
 const app = express()
 
 //mail chimp 
-mailchimp.setConfig({
+client.setConfig({
   apiKey: "4885e419d5c905804f45921a1dba9856-us1",
   server: "us1",
 });
@@ -30,7 +30,7 @@ app.use(express.static("public"))
 
 //listen to the port 
 app.listen(process.env.PORT || port, function(){
-  console.log(`Server is running...`)
+  console.log(`Server is running... `)
 })
 
 //get method to make the html page run on the server
@@ -42,48 +42,35 @@ app.post("/", function(req,res){
   const userFirstName = req.body.firstName
   const userLastName = req.body.lastName
   const userEmail = req.body.email
-  // req.body.name, req.body.email = ""  
 
-  //mail chimp code goes here 
-  const listId = "939f75443f";
-  const subscribingUser = {
-  firstName: userFirstName,
-  lastName: userLastName,
-  email: userEmail
-};
 
-async function run() {
-  const response = await mailchimp.lists.addListMember(listId, {
-    email_address: subscribingUser.email,
-    status: "subscribed",
-    merge_fields: {
-      FNAME: subscribingUser.firstName,
-      LNAME: subscribingUser.lastName
-    }
+client.setConfig({
+  apiKey: "4885e419d5c905804f45921a1dba9856-us1",
+  server: "us1",
+});
+
+const run = async () => {
+  const response = await client.lists.batchListMembers("939f75443f", {
+    members: [{
+      email_address: userEmail,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: userFirstName,
+        LNAME: userLastName
+        },
+    }],
   });
-
-  console.log(
-    `Successfully added contact as an audience member. The contact's id is ${
-      JSON.stringify(response)
-    }.` 
-  );
-}
-
-run();
-
-
   
-  //I can store this data in mail chimp and send automatic emails to my clients 
-  // console.log(userFirstName)
-  // console.log(userLastName)
-  // console.log(userEmail)
-
-
   if (res.statusCode === 200 && userFirstName !== "" && userLastName !== "" && userEmail !== ""){
     res.sendFile(__dirname + "/success.html")
   } else {
     res.sendFile(__dirname + "/failure.html")
   }
+  
+  // console.log(response);
+};
+
+run();
 
 }) 
 
@@ -92,11 +79,16 @@ app.get('/failure', (req, res) => {
 });  
 
 
+//url structure
+//https://<dc>.api.mailchimp.com/3.0/lists/{list_id}/members/{subscriber_hash}/notes/{id}
+//mail chimp url 
+//https://dc.api.mailchimp.com/3.0/lists/{list_id}/members/
 //mail chimp api key 
-//4885e419d5c905804f45921a1dba9856-us1
+//let apiKey = 4885e419d5c905804f45921a1dba9856-us1
+// let dc = us1
 
 //list id
-//939f75443f
+//let listId = 939f75443f
 
 //mail chimp client key (for mobible)
 //983a482eb39da326b29b7870c8fd9c09-us1
